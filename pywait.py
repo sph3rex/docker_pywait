38a2cfa48d6b9928a7e8fe9362763208a34e82a8
sph3rex <sph3rex@users.noreply.github.com>
2018-02-23 13:22:30 +0200

    Update pywait.py

--- pywait.py
+++ pywait.py
@@ -5,8 +5,30 @@ import re
 import sys
 import time
 import socket
+from pprint import pprint
 from urllib.request import urlopen
 from contextlib import closing
+
+
+def log(message):
+    if not args.quiet:
+        print(message)
+
+
+def valid_connection_spec(connection_spec):
+    spec_parts = connection_spec.split(':')
+    valid_protocol_specs = ['tcp', 'udp']
+    if len(spec_parts) != 3:
+        raise ValueError('Invalid spec. Should be (tcp|udp):<name>:<port>.')
+
+    if spec_parts[0] not in valid_protocol_specs:
+        raise ValueError('Invalid spec. Protocol should any of ' + ' '.join(valid_protocol_specs))
+
+    if int(spec_parts[2]) > 65535:
+        raise ValueError('Invalid port spec.')
+
+    return tuple(spec_parts)
+
 
 parser = argparse.ArgumentParser(description='Waits for a service to start')
 subparsers = parser.add_subparsers(help='Specify which type of wait type should we try', dest="command")
@@ -33,30 +55,9 @@ group_exclusive.add_argument('--string', '-s', type=str, help='The string to che
 group_exclusive.add_argument('--regex', '-g', type=str, help='The regex to check for')
 string_check_parser.add_argument('--url', '-u', type=str, help='The URL to check against')
 
-
-def log(message):
-    if not args.quiet:
-        print(message)
-
-
-def valid_connection_spec(connection_spec):
-    spec_parts = connection_spec.split(':')
-    valid_protocol_specs = ['tcp', 'udp']
-    if len(spec_parts) != 3:
-        raise ValueError('Invalid spec. Should be (tcp|udp):<name>:<port>')
-
-    if spec_parts[0] not in valid_protocol_specs:
-        raise ValueError('Invalid spec. Protocol should any of ' + ' '.join(valid_protocol_specs))
-        
-    if spec_parts[2] > 65535:
-        raise ValueError('Invalid port spec.')
-
-    return tuple(spec_parts)
-
-
 # Connection check
 connection_check_parser.add_argument(
-    "--spec", "-p", type=valid_connection_spec,
+    "--spec", "-p", type=valid_connection_spec, action='append',
     help="A spec in the form of (tcp|udp):<name>:<port>"
 )
 connection_check_parser.add_argument('--timeout', '-t', default=2, type=int, help='Default socket timeout')
@@ -78,24 +79,34 @@ if 'string-check' == args.command:
             time.sleep(args.timeout)
 
 if 'connection' == args.command:
+    waits = len(args.spec)
+
     for j in range(1, args.retries + 1):
-        address = args.spec[1]
-        port = int(args.spec[2])
-        proto = socket.SOCK_DGRAM if 'udp' == args.spec[0] else socket.SOCK_STREAM
+        finished_waits = 0
+        waits_error = []
+        for spec in args.spec:
+            address = spec[1]
+            port = int(spec[2])
+            proto = socket.SOCK_DGRAM if 'udp' == spec[0] else socket.SOCK_STREAM
 
-        with closing(socket.socket(socket.AF_INET, proto)) as s:
-            try:
-                s.settimeout(args.timeout)
-                result = s.connect((address, port))
-                s.shutdown(2)
+            with closing(socket.socket(socket.AF_INET, proto)) as s:
+                try:
+                    s.settimeout(args.timeout)
+                    result = s.connect((address, port))
+                    s.shutdown(2)
+                    finished_waits += 1
+                except Exception as ex:
+                    waits_error.append(address + ':' + spec[2] + ' -> ' + str(ex))
+                finally:
+                    s.close()
 
-                log('Done with success.')
-                sys.exit(0)
-            except Exception as ex:
-                log('Failed to connect: ' + str(ex))
-                time.sleep(args.timeout)
-            finally:
-                s.close()
+        if waits != finished_waits:
+            delimiter = "\n\t - "
+            log('Failed to connect: ' + delimiter + delimiter.join(waits_error))
+            time.sleep(args.timeout)
+        else:
+            log('Done with success.')
+            sys.exit(0)
 
 log('Failed waiting. Exiting ...')
 sys.exit(args.exit_code)
